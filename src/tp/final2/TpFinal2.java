578d9d868851111c266fe8ac826106682cb2ce59
Azulws <azul1155cs@hotmail.com>
2018-06-20 18:44:49 -0300

    tp1.03

--- src/tp/final2/TpFinal2.java
+++ src/tp/final2/TpFinal2.java
@@ -22,17 +22,6 @@ public class TpFinal2 {
         System.out.println(uno);
         
         Gson gson = new Gson();
-<<<<<<< HEAD
-        System.out.println("sdaassda");
-=======
-<<<<<<< HEAD
-        
-=======
-        System.out.println("asd");
-        System.out.println("asd");
-    
->>>>>>> parent of 2f8e3c0... sd
->>>>>>> parent of 44b8ff5... Tp1.00
     }
 
 }
