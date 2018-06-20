2f8e3c04e7daaeb4001e80e415e598e1b06ad006
agustin <agustin.caceres96@hotmail.com>
2018-06-20 18:14:05 -0300

    sd
    dd

--- src/tp/final2/TpFinal2.java
+++ src/tp/final2/TpFinal2.java
@@ -24,6 +24,7 @@ public class TpFinal2 {
         Gson gson = new Gson();
         System.out.println("asd");
         System.out.println("asd");
+                System.out.println("hola");
     
     }
 
