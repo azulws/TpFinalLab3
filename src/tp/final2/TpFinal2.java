6dd21a33c364f83b57d33c252dba2e994e3fb1f2
agustin <agustin.caceres96@hotmail.com>
2018-06-23 01:22:20 -0300

    gson pasajeros
    add gson

--- src/tp/final2/TpFinal2.java
+++ src/tp/final2/TpFinal2.java
@@ -1,10 +1,17 @@
 package tp.final2;
 
-//import com.google.gson.Gson;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.UUID;
 import tp.final2.menu.BienvenidoMenu;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
 
 public class TpFinal2 {
 
@@ -30,9 +37,14 @@ public class TpFinal2 {
         ArrayList<Habitacion> habitaciones = new ArrayList<Habitacion>();
         ArrayList<Pasajero> pasajero = new ArrayList<Pasajero>();
         Pasajero p = new Pasajero("agustin", "caceres", 223, "colon 412", "agustin@hotmail.com", "39336482", "argentina");
-        Recepcionista recep = new Recepcionista("agustindominguez95@hotmail.com", "123", "Agustin", "Dominguez", pasajero, habitaciones);
+        Pasajero a = new Pasajero("tomas", "gonzales", 331, "colon 561", "tomas@hotmail.com", "49448231", "argentina");
+        pasajero.add(a);
         pasajero.add(p);
-        System.out.println("P= " + p);
+        Serializar(pasajero);
+//        Recepcionista recep = new Recepcionista("agustindominguez95@hotmail.com", "123", "Agustin", "Dominguez", pasajero, habitaciones);
+//        pasajero.add(p);
+        
+        
 //          try{
 //              recep.ModificarPasajero(p, "39336482");
 //              System.out.println("El pasajero se ha editado exitosamente");
@@ -40,23 +52,45 @@ public class TpFinal2 {
 //          }catch(PasajeroNoEncontradoException e){
 //              System.out.println(e.getMessage());
 //          }
-        Habitacion pepita = new Habitacion(1, Habitacion.Tipo.INDIVIDUAL);
-        Habitacion esme = new Habitacion(2, Habitacion.Tipo.DOBLE);
-        Habitacion dasd = new Habitacion(3, Habitacion.Tipo.SUITES);
-        Habitacion ass = new Habitacion(4, Habitacion.Tipo.TRIPLE);
-        Habitacion pep = new Habitacion(5, Habitacion.Tipo.INDIVIDUAL);
-        habitaciones.add(pepita);
-        habitaciones.add(esme);
-        habitaciones.add(dasd);
-        habitaciones.add(ass);
-        habitaciones.add(pep);
-        System.out.println(habitaciones);
-        recep.checkIn(4);
-        System.out.println(ass.toString());
-        recep.checkout(4);
-        System.out.println(ass.toString());
-        Reserva reserv = new Reserva(pep,p);
-        System.out.println(reserv);
+//        Habitacion pepita = new Habitacion(1, Habitacion.Tipo.INDIVIDUAL);
+//        Habitacion esme = new Habitacion(2, Habitacion.Tipo.DOBLE);
+//        Habitacion dasd = new Habitacion(3, Habitacion.Tipo.SUITES);
+//        Habitacion ass = new Habitacion(4, Habitacion.Tipo.TRIPLE);
+//        Habitacion pep = new Habitacion(5, Habitacion.Tipo.INDIVIDUAL);
+//        habitaciones.add(pepita);
+//        habitaciones.add(esme);
+//        habitaciones.add(dasd);
+//        habitaciones.add(ass);
+//        habitaciones.add(pep);
+//        System.out.println(habitaciones);
+//        recep.checkIn(4);
+//        System.out.println(ass.toString());
+//        recep.checkout(4);
+//        System.out.println(ass.toString());
+//        Reserva reserv = new Reserva(pep,p);
+//        System.out.println(reserv);
+    }
+    
+    public static void Serializar (List pasajero){
+        Gson gson = new Gson();
+        String strJson=gson.toJson(pasajero);
+        
+        FileWriter writer=null;
+        try{
+        writer= new FileWriter("pasajeros.son");
+        writer.write(strJson);
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if(writer != null)
+        {
+            try{
+                writer.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
+    }
 }
